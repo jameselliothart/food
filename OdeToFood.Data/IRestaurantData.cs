aa88fd497def600e3bda61f39398745fe33bd7cc
James Hart <james_hart@qbsol.com>
2019-04-22 09:59:25 -0500

    Add restaurant functionality

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "James' Spot", Location = "Houston", Cuisine = CuisineType.Mexican },
+                new Restaurant {Id = 2, Name = "Trevor's Place", Location = "Austin", Cuisine = CuisineType.Indian },
+                new Restaurant {Id = 3, Name = "Isaiah's Lounge", Location = "San Antonio", Cuisine = CuisineType.Italian},
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
