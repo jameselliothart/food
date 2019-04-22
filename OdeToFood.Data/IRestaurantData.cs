762b2a0e83327ffb306d751423f804b7f889539a
James Hart <james_hart@qbsol.com>
2019-04-22 15:46:32 -0500

    Adding ability to add a restaurant

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,7 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
         int Commit();
     }
 
@@ -40,6 +41,13 @@ namespace OdeToFood.Data
                 restaurant.Cuisine = updatedRestaurant.Cuisine;
             }
             return restaurant;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
         }
 
         public int Commit()
